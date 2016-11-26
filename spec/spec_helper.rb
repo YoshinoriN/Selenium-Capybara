1c80f63ceb8b328f2b6dcb53f67037db7a48f08f
YoshinoriN <nayo-msoft@outlook.jp>
2016-11-26 17:14:47 +0900

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,12 @@ require 'rubygems'
 require 'awesome_print'
 require 'capybara/rspec'
 require 'selenium-webdriver'
-Dir[File.join(Dir.pwd + "/spec/support/**/*.rb")].sort.each {|f| require f}
+Dir[File.join(Dir.pwd + "/spec/pages/**/*.rb")].sort.each {|f| require f}
 
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
+
 Capybara.default_wait_time = 10
 Capybara.app_host = 'http://***.***.***.***'
 
