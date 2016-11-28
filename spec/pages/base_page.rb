18c08e7fc5d6b270337ced7f0648917b06b281e8
YoshinoriN <nayo-msoft@outlook.jp>
2016-11-28 14:16:59 +0900

    Update base_page.rb

--- spec/pages/base_page.rb
+++ spec/pages/base_page.rb
@@ -3,5 +3,14 @@
 class BasePage
   def initialize(capybara_instance)
     @cp = capybara_instance
+    @root_dir = Dir.pwd
+  end
+
+  def virtical_scroll_to(element)
+    script = <<-JS
+      arguments[0].scrollIntoView(true);
+    JS
+    @cp.current_session.driver.browser.execute_script(script, element.native)
+  end    
   end
 end
